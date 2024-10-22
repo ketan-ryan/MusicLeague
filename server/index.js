0f59cbc0db2bbf6ae1f50001e708e31aee9cec9b
ketan-ryan <kyurembeats2@gmail.com>
2024-10-21 22:35:09 -0400

    Retrieve assigned port

--- server/index.js
+++ server/index.js
@@ -251,6 +251,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
-app.listen(port, '0.0.0.0', () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Listening at http://localhost:${port}`)
 });
